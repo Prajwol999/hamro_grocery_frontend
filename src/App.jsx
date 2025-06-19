7500cfb93ce5bdab558b839627a3650fc4115652
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-06-19 18:47:52 +0545

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import NavigationContext from './context/NavigationContext';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import LoginPage from './components/auth/LoginPage';
+import SignupPage from './components/auth/SignupPage';
+import HomePage from './pages/HomePage';
 
+function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+  const navigate = (page) => setCurrentPage(page);
 
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'login': return <LoginPage />;
+      case 'signup': return <SignupPage />;
+      case 'products':
+      case 'contact':
+      case 'about':
+      case 'home':
+      default: return <HomePage />;
+    }
+  };
 
-export const App = () => {
   return (
-    <div><Navbar/>
-    </div>
-  )
+    <NavigationContext.Provider value={{ navigate }}>
+      <Navbar />
+      {renderPage()}
+      {/* <Footer /> */}
+    </NavigationContext.Provider>
+  );
 }
+
 export default App;
+export { NavigationContext, App };
